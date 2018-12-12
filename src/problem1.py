b75732e0e2765a9ba913bf3a1a47881e8bd5b5d1
jarskijr <jarskijr@rose-hulman.edu>
2018-12-11 20:15:07 -0500

    It actually looks good :)

--- src/problem1.py
+++ src/problem1.py
@@ -119,7 +119,7 @@ def problem1(square, thickness, window):
       :type window:    rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.  SEE THE PICTURES in the PDF!
+    # DONE: 3. Implement and test this function.  SEE THE PICTURES in the PDF!
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
